90fa6296eb8730e93c517231c7f8be2cb6d29536
caop <2510154937@qq.com>
2022-05-17 14:07:21 +0800

    初始化

--- src/plugins/storage.js
+++ src/plugins/storage.js
@@ -2,12 +2,20 @@ import Vue from "vue";
 import VueStorage from "vue-ls";
 import pkg from "../../package";
 
-
-// this.$storage
+// this.$ls_local
 Vue.use(VueStorage, {
   namespace: `${pkg.name}_${pkg.version}_`,
-  name: "storage",
+  name: "ls_local",
   storage: "local"    // 存储名称: session, local, memory
 });
 
-export const storage = Vue.storage;
+// this.$ls_session
+Vue.use(VueStorage, {
+  namespace: `${pkg.name}_${pkg.version}_`,
+  name: "ls_session",
+  storage: "session"    // 存储名称: session, local, memory
+});
+
+export const ls_local = Vue.ls_local;
+
+export const ls_session = Vue.ls_session;
